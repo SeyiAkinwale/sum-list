11a98430a79771c3d7bb9d963eda49bf0cb7dd61
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-05-17 18:16:52 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+class Node
+{
+  public:
+    int val;
+    Node * next;
+
+    Node(int initialVal)
+    {
+      val=initialVal;
+      next=nullptr;
+    }
+};
+
+int sumList(Node * head)
+{
+  int sum = 0;
+  Node * current=head;
+  while(current!=nullptr)
+  {
+    sum+=current->val;
+    current=current->next;
+  }
+  return sum;
+}
+
+int main() 
+{
+  Node a(2);
+  Node b(8);
+  Node c(3);
+  Node d(-1);
+  Node e(7);
+  
+  a.next = &b;
+  b.next = &c;
+  c.next = &d;
+  d.next = &e;
+  cout<<sumList(a);
+}
